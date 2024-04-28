f835b7be0a11f3cb76693feb995fa4edf5c55add
Andrey <andydeshko@gmail.com>
2024-04-28 14:39:34 +0300

    Iterator added explanation as comments

--- Iterator/src/main/java/org/example/FibonacciIterator.java
+++ Iterator/src/main/java/org/example/FibonacciIterator.java
@@ -4,18 +4,20 @@ import java.util.Iterator;
 
 
 public class FibonacciIterator implements Iterator<Integer> {
+    // first inital values for Fibonacci sequence
     private int current = 0;
     private int next = 1;
 
     @Override
     public boolean hasNext() {
-        return true;
+        return true;  // Fibonacci sequence can be infinite, so always return true
     }
 
     @Override
     public Integer next() {
         int result = current;
         int temp = current + next;
+        // Update current and next numbers for further calculations
         current = next;
         next = temp;
         return result;
